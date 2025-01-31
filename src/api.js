e7748794a70995535e64b447f2c780a6319ba391
guilherme_celente <guilherme.celente@outlook.com>
2025-01-31 11:21:48 -0300

    chatbot test

--- src/api.js
+++ src/api.js
@@ -1,7 +1,29 @@
 import express from "express";
 import cors from "cors";
 import apiRoutes from "./routes/apiRoutes.js"
+import { Client, LocalAuth } from 'whatsapp-web.js';
+import qrcode from 'qrcode-terminal';
 
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+      headless: true,  // Se desejar, pode deixar como false para ver o navegador
+    }
+});
+  
+  // Quando o cliente estiver pronto, exibe uma mensagem de confirmação
+client.on('ready', () => {
+    console.log('Cliente do WhatsApp pronto!');
+});
+  
+  // Quando o cliente precisar gerar o QR Code para conexão
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true }); // Gera o QR Code no terminal
+});
+  
+  // Inicia o cliente
+client.initialize();
 
 const app = express();
 
@@ -14,3 +36,4 @@ app.use('/api', apiRoutes)
 app.listen(3000, () => {
     console.log('Servidor Rodando em http://localhost:3000')
 })
+
