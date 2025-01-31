df41b6322c4d9b9a2d89da6f52314aee458ab0fc
guilherme_celente <guilherme.celente@outlook.com>
2025-01-31 14:37:38 -0300

    att chatbot

--- src/config/chatbotConfig.js
+++ src/config/chatbotConfig.js
@@ -1,29 +1,5 @@
 // src/config/chatbotConfig.js
 import { Client } from 'whatsapp-web.js';
-//import chrome from 'chrome-aws-lambda';
-//import puppeteer from 'puppeteer-core';
-/*
-async function initializeClient() {
-    // Aguarde a resolução do caminho do Chromium
-    const executablePath = await chrome.executablePath;
-
-    const client = new Client({
-        puppeteer: {
-            args: chrome.args,
-            executablePath, // Caminho resolvido
-            headless: chrome.headless,
-        }
-    });
-
-    client.on('ready', () => {
-        console.log('WhatsApp conectado!');
-    });
-
-    // Inicialize o cliente
-    await client.initialize(); // Espera a inicialização ser concluída antes de prosseguir
-
-    return client;
-}*/
 
 const client = new Client();
 
@@ -31,7 +7,6 @@ client.on('ready', () => {
     console.log('WhatsApp conectado!');
 });
 
-client.initialize()
-
+client.initialize();
 
 export default client;
--- src/routes/chatbotRoutes.js
+++ src/routes/chatbotRoutes.js
@@ -6,15 +6,26 @@ import { processMessages } from "../controllers/chatbotController.js";
 
 const chatRouter = express.Router();
 
+// Variável para garantir que o QR code só seja enviado uma vez
+let qrSent = false;
+
 // Rota que renderiza o HTML com o QR code
 chatRouter.get('/qr', (req, res) => {
+    // Se o QR Code já foi enviado, não faça nada
+    if (qrSent) {
+        return;
+    }
+
     client.on('qr', async (qr) => {
         try {
             // Gerar QR Code como base64
             const qrImage = await qrcode.toDataURL(qr);
-            
+
             // Renderizar o template EJS, passando o QR code gerado
             res.render('qrPage', { qrCode: qrImage });
+
+            // Marcar como enviado para não enviar novamente
+            qrSent = true;
         } catch (error) {
             res.status(500).json({ error: 'Falha ao gerar o QR Code' });
         }
