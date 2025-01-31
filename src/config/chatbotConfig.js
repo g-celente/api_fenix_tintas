345bab7a087458c52b63aa654f04a6a650e0d06a
guilherme_celente <guilherme.celente@outlook.com>
2025-01-31 13:13:50 -0300

    chatbot test2

--- src/config/chatbotConfig.js
+++ src/config/chatbotConfig.js
@@ -1,7 +1,15 @@
 // src/config/chatbotConfig.js
 import { Client } from 'whatsapp-web.js';
+import chrome from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
 
-const client = new Client();
+const client = new Client({
+    puppeteer: {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+    }
+});
 
 client.on('ready', () => {
     console.log('WhatsApp conectado!');
